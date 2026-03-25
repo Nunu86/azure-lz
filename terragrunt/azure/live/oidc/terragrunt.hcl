77211aabc17cc83551833fbb44831a3ce9e8f8a5
Nunu86 <nanaokorie@gmail.com>
2026-03-25 12:18:26 +0000

    25th test

--- terragrunt/azure/live/oidc/terragrunt.hcl
+++ terragrunt/azure/live/oidc/terragrunt.hcl
@@ -13,7 +13,7 @@ locals {
 
 inputs = {
   azure_oidc_config = {
-    mgmt = {
+    workflow_dispatch = {
       issuer_url = "https://token.actions.githubusercontent.com"
       subject    = "repo:Nunu86/azure-lz:ref:refs/heads/main"
       audiences  = ["api://AzureADTokenExchange"]
@@ -38,29 +38,13 @@ inputs = {
       ]
     },
 
-    mgmt2 = {
+    pull_request = {
       issuer_url = "https://token.actions.githubusercontent.com"
       subject    = "repo:Nunu86/azure-lz:pull_request"
       audiences  = ["api://AzureADTokenExchange"]
 
-      role_assignments = [
-        {
-          role_definition_name = "Management Group Contributor"
-          scope                = local.mg_policy_scope
-        },
-        {
-          role_definition_name = "Contributor"
-          scope                = local.mg_policy_scope
-        },
-        {
-          role_definition_name = "User Access Administrator"
-          scope                = local.mg_policy_scope
-        },
-        {
-          role_definition_name = "Storage Blob Data Contributor"
-          scope = "/subscriptions/${include.root.locals.remote_state_subscription.id}/resourceGroups/terraform-state-backend/providers/Microsoft.Storage/storageAccounts/terraformstatebacken"
-        }
-      ]
+      role_assignments = []
+       
     }
   }
 }
--- terragrunt/azure/modules/terraform_azure_oidc/main.tf
+++ terragrunt/azure/modules/terraform_azure_oidc/main.tf
@@ -16,25 +16,31 @@ role_assignment_map = {
   }
 
 }
-
+#one service app
 resource "azuread_application" "oidc" {
-  for_each = var.azure_oidc_config
+  #for_each = var.azure_oidc_config
 
-  display_name = "azure-oidc-${each.key}"
+  display_name = "azure-oidc-app"
   identifier_uris = []
   
 }
 
-
+/*
 resource "azuread_service_principal" "oidc" {
   for_each = azuread_application.oidc
   client_id = each.value.client_id
+}
+*/
+#one service principal
+resource "azuread_service_principal" "oidc" {
+  client_id = azuread_application.oidc.client_id
 }
 
 resource "azuread_application_federated_identity_credential" "oidc" {
   for_each              = var.azure_oidc_config
 
-  application_id = azuread_application.oidc[each.key].id
+  #application_id = azuread_application.oidc[each.key].id
+  application_id = azuread_application.oidc.id
   display_name          = "github-oidc-${each.key}"
   description           = "GitHub OIDC Federated identity"
 
@@ -42,12 +48,22 @@ resource "azuread_application_federated_identity_credential" "oidc" {
   issuer    = each.value.issuer_url
   subject   = each.value.subject
 }
-
+/*
 resource "azurerm_role_assignment" "oidc" {
   for_each = local.role_assignment_map
 
   scope              = each.value.scope
   role_definition_name = each.value.role_definition_name
   principal_id       = azuread_service_principal.oidc[each.value.key].object_id
+}
+*/
+
+resource "azurerm_role_assignment" "oidc" {
+  for_each = local.role_assignment_map
+
+  scope                = each.value.scope
+  role_definition_name = each.value.role_definition_name
+
+  principal_id = azuread_service_principal.oidc.object_id
 }
 
